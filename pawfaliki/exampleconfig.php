5390fd249365517e9ba1203d0c304f0c0b664982
Dan Bethell <dan@pawfal.org>
2005-06-15 17:45:41 +0000

    Added an example external config file.
    git-svn-id: http://pawfaliki.svn.sourceforge.net/svnroot/pawfaliki/trunk@22 fb719c85-6421-0410-adb9-d7feda6f15fd

---
+++ pawfaliki/exampleconfig.php
@@ -0,0 +1,11 @@
+<?php
+//=========================
+// An example of how to override a few of the Pawfaliki configuration settings
+//=========================
+$config['GENERAL']['TITLE'] = "Example Wiki"; // a new wiki title
+$config['GENERAL']['HOMEPAGE'] = "An Example"; // a new homepage title
+$config['GENERAL']['DEBUG'] = true; // display debug info
+$config['SYNTAX']['HTMLCODE'] = true; // allow HTML in wiki code
+$config['BACKUP']['ENABLE'] = false; // disable backup & restore
+$config['MISC']['EXTERNALLINKS_NEWWINDOW']  = true; // open external links in a new window
+?>
